5584e98b43a9c5b9702d94c5bba2e3a4ee3b5447
Balajiu97 <yuvabala355@gmail.com>
2025-07-31 14:58:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/app
-#aws eks update-kubeconfig --region ap-south-1 --name my-cluster-v2
+aws eks update-kubeconfig --region ap-south-1 --name my-cluster-v2
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml 
