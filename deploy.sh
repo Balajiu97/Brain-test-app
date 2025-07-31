ccaab2aebee8a1a1ded1d34af048814ed39bad62
Balajiu97 <yuvabala355@gmail.com>
2025-07-31 15:02:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
 cd /home/ubuntu/app
-aws eks update-kubeconfig --region ap-south-1 --name my-cluster-v2
+
+# Set kubeconfig path (save under ec2-user's home)
+export KUBECONFIG=/home/ubuntu/.kube/config
+
+# Make sure .kube directory exists for ec2-user
+mkdir -p /home/ubuntu/.kube
+
+# Set correct kubeconfig from EKS
+aws eks update-kubeconfig \
+  --region ap-south-1 \
+  --name my-cluster-v2 \
+  --kubeconfig /home/ubuntu/.kube/config
+
+# (Optional) Test connection
+kubectl cluster-info
+
+# Deploy
 kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml 
+kubectl apply -f service.yaml
